96198940cd9e03f2459d4c3a7a14d737d5fb1825
ale.spu77 <ale.spu77@gmail.com>
2025-02-07 17:24:59 +0000

    cambios en el codigo foter header fondos

--- src/types.d.ts
+++ src/types.d.ts
@@ -183,7 +183,7 @@ export interface Disclaimer {
 
 // COMPONENTS
 export interface CallToAction extends Omit<HTMLAttributes<'a'>, 'slot'> {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'link' | 'whatsapp'| 'formulario' | 'fancy';
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link' | 'whatsapp'| 'formulario' | 'fancy' | 'formulario2';
   text?: string;
   icon?: string;
   classes?: Record<string, string>;
